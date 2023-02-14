ab9f92e91ac74b7aca054993759a5ec1128ee357
Pearl <pearlkhinkhin.15@gmail.com>
2023-02-14 14:24:11 +0800

    fixed feature1 buges (skill result horizontal, set goal click disabled

---
+++ Downloads/techsmvp-KaungFixed/src/main/java/nus/iss/team2/ADProjectTECHS/security/MvcConfig.java
@@ -0,0 +1,35 @@
+package nus.iss.team2.ADProjectTECHS.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/403").setViewName("Others/403");
+        registry.addViewController("/login").setViewName("Others/login");
+    }
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/coffe")
+//                .allowedOrigins("http://localhost:5000")
+//                .allowedMethods("GET","POST", "PUT", "DELETE")
+//                .allowCredentials(true);
+//
+//    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/avatar/**")
+                .addResourceLocations("file:"+System.getProperty("user.dir")
+                        +"/src/main/resources/static/images/avatar/");
+    }
+}
