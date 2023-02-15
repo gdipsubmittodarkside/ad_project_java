32cbfef452ac5725f0213c98fbca1242799a98e8
Huigamlong <legendarylong303@gmail.com>
2023-02-15 13:03:28 +0800

    fix calendar

--- Downloads/techsmvp-KaungFixed/src/main/java/nus/iss/team2/ADProjectTECHS/AdProjectTechsApplication.java
+++ Downloads/techsmvp-KaungFixed/src/main/java/nus/iss/team2/ADProjectTECHS/AdProjectTechsApplication.java
@@ -1,38 +1,21 @@
 package nus.iss.team2.ADProjectTECHS;
 
-import java.time.LocalDate;
-import java.util.List;
-
-import nus.iss.team2.ADProjectTECHS.Model.Enums.Gender;
-import nus.iss.team2.ADProjectTECHS.Service.MemberService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import nus.iss.team2.ADProjectTECHS.Controller.DeveloperController.DeveloperController;
-import nus.iss.team2.ADProjectTECHS.Model.CourseCrawled;
-import nus.iss.team2.ADProjectTECHS.Model.Enums.Education;
-import nus.iss.team2.ADProjectTECHS.Model.Job;
-import nus.iss.team2.ADProjectTECHS.Model.JobSkill;
-import nus.iss.team2.ADProjectTECHS.Model.Member;
-import nus.iss.team2.ADProjectTECHS.Model.MyCourse;
-import nus.iss.team2.ADProjectTECHS.Model.MySkill;
-import nus.iss.team2.ADProjectTECHS.Model.ScheduleEvent;
-import nus.iss.team2.ADProjectTECHS.Model.Skill;
 import nus.iss.team2.ADProjectTECHS.Repository.CourseCrawledRepository;
 import nus.iss.team2.ADProjectTECHS.Repository.JobRepository;
 import nus.iss.team2.ADProjectTECHS.Repository.JobSkillRepository;
-import nus.iss.team2.ADProjectTECHS.Repository.MemberRepository;
 import nus.iss.team2.ADProjectTECHS.Repository.MyCourseRepository;
 import nus.iss.team2.ADProjectTECHS.Repository.MySkillRepository;
 import nus.iss.team2.ADProjectTECHS.Repository.ScheduleEventRepository;
 import nus.iss.team2.ADProjectTECHS.Repository.SkillRepository;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import nus.iss.team2.ADProjectTECHS.Service.MemberService;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
