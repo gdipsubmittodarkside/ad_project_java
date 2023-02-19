436d2f9eeb82881d758a06af3f87b11f8bc1263e
PhoebsOnEarth <oooopsphoebe@gmail.com>
2023-02-19 09:54:15 +0800

    fix settings

--- Downloads/techsmvp-KaungFixed/src/main/java/nus/iss/team2/ADProjectTECHS/Controller/SettingController.java
+++ Downloads/techsmvp-KaungFixed/src/main/java/nus/iss/team2/ADProjectTECHS/Controller/SettingController.java
@@ -73,9 +73,6 @@ public class SettingController {
         Job dreamJob = currentMember.getDreamJob();
 
 
-        
-
-
         if (currentMember == null) 
             throw new RuntimeException("cannot find current member");
 
@@ -112,14 +109,6 @@ public class SettingController {
 
         if (newMember != null) {
 
-//            if (newMember.getUsername()!=null && newMember.getUsername().trim()!="") {
-//                currentMember.setUsername(newMember.getUsername());
-//
-//            }
-
-//            if (newMember.getEmail() != null && newMember.getEmail().trim() != "" ) {
-//                currentMember.setEmail(newMember.getEmail());
-//            }
 
             if (newMember.getCurrentJobTitle()!=null && newMember.getCurrentJobTitle().trim()!="") {
                 currentMember.setCurrentJobTitle(newMember.getCurrentJobTitle());
@@ -144,14 +133,6 @@ public class SettingController {
 
         model.addAttribute("message", "saved profile");
 
-
-//        if (newMember.getUsername()!=null && newMember.getUsername().trim()!="") {
-//
-//            UserDetails userDetails = userDetailsService.loadUserByUsername(currentMember.getUsername());
-//            MemberUtils.setLoginUser(userDetails);
-//
-//            return "redirect:/logout";
-//        }
 
         return "redirect:/settings";
 
@@ -221,20 +202,21 @@ public class SettingController {
 
 
         if (curPwd.equals(newPwd)) {
-            model.addAttribute("message", "Your new password must be different than the old one.");
-            model.addAttribute("member" ,new Member());
-            return "Others/settings";
+
+            ra.addFlashAttribute("message", "Your new password must be different than the old one.");
+            return "redirect:/settings";
 
         }
+
+
         if (!passwordEncoder.matches(curPwd, currentMember.getPassword())) {
-            model.addAttribute("message", "Your old password is incorrect.");
-            model.addAttribute("member" ,new Member());
-            return "Others/settings";
+            ra.addFlashAttribute("message", "Your old password is incorrect.");
+            return "redirect:/settings";
 
         } else {
 
             memberService.changePassword(currentMember, newPwd);
-            model.addAttribute("message","You have changed your password successfully. Please login again." );
+            ra.addAttribute("message","You have changed your password successfully. Please login again." );
             return "redirect:/logout";
         }
     }
@@ -287,190 +269,7 @@ public class SettingController {
     }
 
 
-//
-//    @GetMapping(value = {"/",""})
-//    public String showSettingsPage(Model model){
-//
-//        // if (userId < 0) {
-//        //     Member currentMember = getMemberFromSpringSecurity();
-//        //     userId = currentMember.getMemberId();
-//        // }
-//
-//         //find member
-//        String currentUsername = MemberUtils.getMemberFromSpringSecurity();
-//
-//        Member currentMember = memberService.loadMemberByUsername(currentUsername);
-//
-//
-//        if (currentMember == null) throw new RuntimeException("cannot find current member");
-//
-//        model.addAttribute("member", new Member());
-//        List<Job> jobList = jobService.findAll();
-//        model.addAttribute("jobList", jobList);
-//
-//        List<Skill> skillList = skillService.findAll();
-//        model.addAttribute("skillList", skillList);
-//
-//        return "Others/settings";
-//    }
-//
-//    @PostMapping("/profile")
-//    public String updateProfile(@ModelAttribute("member") Member newMember, RedirectAttributes redirectAttributes,Model model){
-//        // Member currentMember = getCurrentMember(userId);
-//
-//        String currentUsername = MemberUtils.getMemberFromSpringSecurity();
-//
-//        Member currentMember = memberService.loadMemberByUsername(currentUsername);
-//
-//
-//        if (newMember != null) {
-//
-//            if (newMember.getUsername()!=null && newMember.getUsername().trim()!="") {
-//                currentMember.setUsername(newMember.getUsername());
-//
-//            }
-//
-//            if (newMember.getEmail() != null && newMember.getEmail().trim() != "" ) {
-//                currentMember.setEmail(newMember.getEmail());
-//            }
-//
-//            if (newMember.getCurrentJobTitle()!=null && newMember.getCurrentJobTitle().trim()!="") {
-//                currentMember.setCurrentJobTitle(newMember.getCurrentJobTitle());
-//            }
-//
-//            if (newMember.getBirthday()!=null) {
-//                currentMember.setBirthday(newMember.getBirthday());
-//            }
-//
-//            if (newMember.getShortBio()!=null && newMember.getShortBio().trim()!="") {
-//                currentMember.setShortBio(newMember.getShortBio());
-//            }
-//
-//            if (newMember.getDreamJob()!=null) {
-//                currentMember.setDreamJob(newMember.getDreamJob());
-//
-//            }
-//
-//
-//            memberService.save(currentMember);
-//
-//        }
-//
-//        model.addAttribute("message", "saved profile");
-//
-//
-//        if (newMember.getUsername()!=null && newMember.getUsername().trim()!="") {
-//
-//            return "redirect:/logout";
-//        }
-//
-//        return "redirect:/settings";
-//
-//    }
-//
-//    @PostMapping("/information")
-//    public String updateInfo(@ModelAttribute("member") Member newMember, @RequestParam("skills") List<Integer> skillsIds , RedirectAttributes redirectAttributes, Model model){
-//        // Member currentMember = getCurrentMember(userId);
-//
-//        String currentUsername = MemberUtils.getMemberFromSpringSecurity();
-//
-//        Member currentMember = memberService.loadMemberByUsername(currentUsername);
-//
-//
-//        if (skillsIds.size()>0) {
-//            List<Skill> skillList = new ArrayList<>();
-//            for (int i = 0; i < skillsIds.size(); i++) {
-//                Skill skill = skillService.findSkillById(Long.valueOf(skillsIds.get(i)));
-//                MySkill mySkill = new MySkill();
-//                if (mySkillService.findMySkillByMemberAndSkill(currentMember, skill) == null){
-//                    mySkill.setSkill(skill);
-//                    mySkill.setMember(currentMember);
-//                    mySkillService.save(mySkill);
-//                    currentMember.getMySkills().add(mySkill);
-//                }
-//
-//            }
-//        }
-//
-//
-//        if (newMember!=null) {
-//
-//            if (newMember.getGender() != null) {
-//                currentMember.setGender(newMember.getGender());
-//            }
-//            if (newMember.getEducation() != null) {
-//                currentMember.setEducation(newMember.getEducation());
-//            }
-//
-//            memberService.save(currentMember);
-//        }
-//
-//        model.addAttribute("message", "save info");
-//
-//        return "redirect:/settings";
-//
-//    }
-//
-//
-//    @PostMapping("/password")
-//    public String updatePassword(
-//            @RequestParam("curPwd")String curPwd,
-//            @RequestParam("newPwd")String newPwd,
-//            RedirectAttributes ra,
-//            Model model){
-//        // Member currentMember = getCurrentMember(userId);
-//        String currentUsername = MemberUtils.getMemberFromSpringSecurity();
-//
-//        Member currentMember = memberService.loadMemberByUsername(currentUsername);
-//
-//
-//        if (curPwd.equals(newPwd)) {
-//            model.addAttribute("message", "Your new password must be different than the old one.");
-//            model.addAttribute("member" ,new Member());
-//            return "settings";
-//
-//        }
-//        if (!passwordEncoder.matches(curPwd, currentMember.getPassword())) {
-//            model.addAttribute("message", "Your old password is incorrect.");
-//            model.addAttribute("member" ,new Member());
-//            return "settings";
-//
-//        } else {
-//
-//            memberService.changePassword(currentMember, newPwd);
-//            model.addAttribute("message","You have changed your password successfully. Please login again." );
-//            return "redirect:/logout";
-//        }
-//    }
-//
-//
-//
-//
-//    @RequestMapping("/image")
-//    @ResponseBody
-//    public String uploadImage(@RequestParam("file")MultipartFile file, HttpServletRequest request) {
-//        String staticPath = ClassUtils.getDefaultClassLoader().getResource("static").getPath();
-//        String filename = file.getOriginalFilename();
-//        String url_path = "images/avatar" + File.separator + filename;
-//        String savePath = staticPath + File.separator + url_path;
-//        String visitPath = "static/" + url_path;
-//        File saveFile = new File(savePath);
-//        if (!saveFile.exists()){
-//            saveFile.mkdir();
-//        }
-//        try {
-//            file.transferTo(saveFile);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        return visitPath;
-//    }
-//
-//
-//    public Member getCurrentMember(Long id) {
-//        return memberService.findById(id);
-//    }
+
     public Boolean setAuth2Avatar(String visitPath) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
